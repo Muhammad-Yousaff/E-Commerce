30078c3fe8638a36ae358c1cffba9f7fe15458ac
Muhammad yousaf <muhammad.yousaf6239@gmail.com>
2024-05-05 08:09:47 -0700

    Implement Navbar with logo, shop category links, cart icon, login button; integrated routing via react-router-dom and dynamic active menu state

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+
+// import './App.css'
+// import Navbar from './Components/Navbar/Navbar'
+// import { BrowserRouter ,Routes , Route } from 'react-router-dom'
+// import Shop from './Components/Pages/Shop'
+// import ShopCategory from './Components/Pages/ShopCategory'
+// import Product from './Components/Pages/Product'
+// import Cart from './Components/Pages/Cart'
+// const App = () => {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//       <Navbar/>
+//       <Routes>
+//         <Route path='/' element={<Shop/>}/>
+//         <Route path='/mens' element={<ShopCategory Category="men"/>}/>
+//         <Route path='/women' element={<ShopCategory Category="women"/>}/>
+//         <Route path='/product' element={<Product Category="Product"/>}/>
+//         <Route path='/productId' element={<Product/>}/>
+//         <Route path='/cart' element={<Cart/>}/>
+//         <Route path='/login' element={<LoginSignUp/>}/>
+//       </Routes>
+
+//       </BrowserRouter>
+//     </div>
+//   )
+// }
+
+// export default App
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Shop from './Components/Pages/Shop';
+import ShopCategory from './Components/Pages/ShopCategory';
+import Product from './Components/Pages/Product';
+import Cart from './Components/Pages/Cart';
+import LoginSignUp from './Components/Pages/LoginSignUp'; // Ensure this import exists
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/mens" element={<ShopCategory Category="men" />} />
+        <Route path="/women" element={<ShopCategory Category="women" />} />
+        <Route path="/product" element={<Product Category="Product" />} />
+        <Route path="/productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignUp />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
